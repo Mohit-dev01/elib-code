7931bba1ce1aadf6c8334ccd911e7389d130dacb
Mohit Wadhwa <mohitwadhwa7788@gmail.com>
2025-08-08 16:30:04 +0530

    validate user request body

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import config from "./config/config";
 import connectDb from "./config/db";
 import globalErrorHandler from "./middleware/globalErrorHandler";
 import userRouter from "./user/userRouter";
+import express from "express";
 
 const startServer = async () => {
   const PORT = config.port;
@@ -10,6 +11,8 @@ const startServer = async () => {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
+
+  app.use(express.json());
 
   app.use("/api/users", userRouter);
   app.use(globalErrorHandler);
