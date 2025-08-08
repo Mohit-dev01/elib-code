637808a5d33cae141c447991e0940207570f068b
Mohit Wadhwa <mohitwadhwa7788@gmail.com>
2025-08-08 15:24:56 +0530

    add database setup

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,6 +3,7 @@ dotenv.config();
 
 const config = {
   port: process.env.PORT,
+  MONGO_DB_URI: process.env.MONGO_DB_URI || "",
 };
 
 export default config;
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import app from "./app";
 import config from "./config/config";
+import connectDb from "./config/db";
 
-const PORT = config.port;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+const startServer = async () => {
+  const PORT = config.port;
+  await connectDb();
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
+startServer();
