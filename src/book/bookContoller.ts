cfffc63919fca2030607a45844e159cbdf338d91
Mohit Wadhwa <mohitwadhwa7788@gmail.com>
2025-08-09 13:14:23 +0530

    setup cloudinary for image and pdf

--- src/book/bookContoller.ts
+++ src/book/bookContoller.ts
@@ -1,7 +1,49 @@
 import { Request, Response, NextFunction } from "express";
+import cloudinary from "../config/cloudinary";
+import path from "node:path";
+import createHttpError from "http-errors";
 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
-  const {} = req.body;
-  return res.json({ message: "Ok" });
+  console.log("files", req.files);
+
+  try {
+    const files = req.files as { [fieldname: string]: Express.Multer.File[] };
+    const coverImageMimetype = files.coverImage
+      ? files.coverImage[0]?.mimetype.split("/").at(-1)
+      : "";
+    const fileName = files.coverImage ? files.coverImage[0]?.filename : "";
+    const filePath = path.resolve(
+      __dirname,
+      "../../public/uploads",
+      fileName as string
+    );
+    const uploadResult = await cloudinary.uploader.upload(filePath, {
+      public_id: fileName || "",
+      folder: "book_covers",
+      format: coverImageMimetype || "jdg",
+    });
+
+    const bookFileName = files.file ? files.file[0]?.filename : "";
+
+    const bookFilePath = path.resolve(
+      __dirname,
+      "../../public/uploads",
+      bookFileName as string
+    );
+
+    const uploadBook = await cloudinary.uploader.upload(bookFilePath, {
+      resource_type: "raw",
+      filename_override: bookFileName || "",
+      folder: "book_pdf",
+      format: "pdf",
+    });
+    console.log("uploadResult", uploadResult);
+    console.log("uploadBook", uploadBook);
+
+    return res.json({ message: "Ok" });
+  } catch (error) {
+    console.error("Error uploading file:", error);
+    return next(createHttpError(400, `Error uploading file ${error}`));
+  }
 };
 export { createBook };
--- src/config/config.ts
+++ src/config/config.ts
@@ -6,6 +6,9 @@ const config = {
   MONGO_DB_URI: process.env.MONGO_DB_URI || "",
   NODE_ENV: process.env.NODE_ENV || "development",
   JWT_SECRET: process.env.JWT_SECRET,
+  CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+  CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
 };
 
 export default config;
