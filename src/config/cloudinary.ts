be6feb19c4054e3c31c1a8136afc6ef000a26cd2
Mohit Wadhwa <mohitwadhwa7788@gmail.com>
2025-08-10 16:25:20 +0530

    code restructure for monorepo

--- src/config/cloudinary.ts
+++
@@ -1,9 +0,0 @@
-import { v2 as cloudinary } from "cloudinary";
-import config from "./config";
-cloudinary.config({
-  cloud_name: config.CLOUDINARY_CLOUD_NAME as string,
-  api_key: config.CLOUDINARY_API_KEY as string,
-  api_secret: config.CLOUDINARY_API_SECRET as string,
-});
-
-export default cloudinary;
